1e7b964d8640ef2159f5c00ffa9063cd259e17d0
Júlio Figueiredo <julioo.1amorim@gmail.com>
2024-10-14 19:36:35 -0300

    docs: add python notes

---
+++ Intermediário/Básicos/List.py
@@ -0,0 +1,4 @@
+lista = [123, True, 'julio', 1.2, []]
+
+print(lista)
+print(lista[2])
---
+++ Intermediário/Básicos/loopfor.py
@@ -0,0 +1,10 @@
+texto = 'Python'
+
+for letra in texto:
+    print(letra)
+    
+# range -> range (star, stop, step)
+    
+    
+for i in range(0, 10, 2): # vai de 0 a 9
+    print(i)
